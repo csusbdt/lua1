d6d63a1aa8e086588a34eae5cb7e39881b915f7a
David Turner <csusbdt@gmail.com>
2014-05-26 14:19:12 -0700

    X

--- working/init.lua
+++ working/init.lua
@@ -1,92 +1,87 @@
 -- Test fonts.
 
-local font1, err = open_font('fonts/I_dont_exist.ttf')
-assert(not font1)
-assert(err and type(err) == 'string')
+local f, f2, t, t2, w, w2, wi, w2i
 
-font1, err = open_font('fonts/I_dont_exist.ttf', 24)
-assert(not font1)
-assert(err and type(err) == 'string')
+f, err = open_font('fonts/I_dont_exist.ttf')
+assert(not f and type(err) == 'string')
 
-font1, err = open_font(24, 'fonts/DroidSansMono.ttf')
-assert(not font1)
-assert(err and type(err) == 'string')
+f, err = open_font('fonts/I_dont_exist.ttf', 24)
+assert(not f and type(err) == 'string')
 
-font1, err = open_font('fonts/DroidSansMono.ttf', 'not a number')
-assert(not font1)
-assert(err and type(err) == 'string')
+f, err = open_font(24, 'fonts/DroidSansMono.ttf')
+assert(not f and type(err) == 'string')
+
+f, err = open_font('fonts/DroidSansMono.ttf', 'not a number')
+assert(not f and type(err) == 'string')
 
 --close_font() -- This opens a popup from the engine side.
 --close_font('not userdata') -- This opens a popup from the engine side.
 
-font1, err = open_font('fonts/DroidSansMono.ttf', 24)
-assert(font1 and type(font1) == 'userdata')
-assert(not err)
+f, err = open_font('fonts/DroidSansMono.ttf', 24)
+assert(type(f) == 'userdata' and not err)
 
-font2, err = open_font('fonts/DroidSansMono.ttf', 24)
-assert(font2 and type(font2) == 'userdata')
-assert(not err)
-assert(font1 ~= font2)
-close_font(font2)
+f2, err = open_font('fonts/DroidSansMono.ttf', 24)
+assert(type(f2) == 'userdata')
+assert(f ~= f2)
+close_font(f2)
 
 -- Test textures.
 
-local texture1, err = texture_from_file(1, 2)
-assert(texture1 == nil and type(err) == 'string')
+t, err = texture_from_file(1, 2)
+assert(not t and type(err) == 'string')
 
-texture1, err = texture_from_file(1)
-assert(texture1 == nil and type(err) == 'string')
+t, err = texture_from_file(1)
+assert(not t and type(err) == 'string')
 
-texture1, err = texture_from_file('textures/I_dont_exist.png')
-assert(texture1 == nil and type(err) == 'string')
+t, err = texture_from_file('textures/I_dont_exist.png')
+assert(not t and type(err) == 'string')
 
-texture1, w, h = texture_from_file('textures/Cara.png')
-assert(type(texture1) == 'userdata' and type(w) == 'number' and type(h) == 'number')
+t, w, h = texture_from_file('textures/Cara.png')
+assert(type(t) == 'userdata' and type(w) == 'number' and type(h) == 'number')
 
-local texture2 = texture_from_file('textures/Cara.png')
-assert(type(texture2) == 'userdata' and texture1 ~= texture2)
+t2 = texture_from_file('textures/Cara.png')
+assert(type(t2) == 'userdata' and t~= t2)
 
-err = destroy_texture(texture2)
+err = destroy_texture(t2)
 assert(err == nil)
 
 err = destroy_texture('Not a texture')
 assert(type(err) == 'string')
 
-err = render_texture(texture1)
+err = render_texture(t)
 assert(err == nil)
-render_texture(texture1, 100, 100)
-render_texture(texture1, 200, 200, 32, 32)
-render_texture(texture1, 50, 50, 100, 100, 300, 300)
-render_texture(texture1, 50, 50, 100, 100, 400, 300, 32, 32)
+render_texture(t, 100, 100)
+render_texture(t, 200, 200, 32, 32)
+render_texture(t, 50, 50, 100, 100, 300, 300)
+render_texture(t, 50, 50, 100, 100, 400, 300, 32, 32)
+
+render_texture(
+	texture_from_font(f, "Testing 1 2 3 ..."),
+	20, 20
+)
 
 render_present()
 
 -- Test audio.
 
-local wave1, err = wave_from_file('waves/I_dont_exist.wav')
-assert(not wave);
-assert(err and type(err) == 'string')
+local w, err = wave_from_file('waves/I_dont_exist.wav')
+assert(not w and type(err) == 'string')
 
-wave1, err = wave_from_file('I_dont_exist/Overworld.wav')
-assert(not wave);
-assert(err and type(err) == 'string')
+w, err = wave_from_file('I_dont_exist/Overworld.wav')
+assert(not w and type(err) == 'string')
 
-wave1, err = wave_from_file('waves/Overworld.wav')
-assert(wave1);
-assert(not err);
+w, err = wave_from_file('waves/Overworld.wav')
+assert(w and not err);
 
-local wave1_i, err = loop_wave(wave1)
-assert(wave1_i)
-assert(not err)
+wi, err = loop_wave(w)
+assert(wi and not err)
 
-local wave2_i, err = loop_wave(nil)
-assert(not wave2_i)
-assert(err)
+w2i, err = loop_wave(nil)
+assert(not w2i and err)
 
 -- Test on touch event.
 
 function on_touch(x, y)
 	print('x = ' .. x .. '  y = ' .. y)
-	render_present()
 end
 
